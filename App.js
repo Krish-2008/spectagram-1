3a469be377867316b9da60c6ad66bb862ea280ef
Krish-2008 <89650110+Krish-2008@users.noreply.github.com>
2022-06-02 14:19:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {View,StyleSheet,Text} from 'react-native'
+
+export default function App(){
+    <View style={styles.Container}>
+    <Text>App</Text>
+    </View>
+}
---
+++ Screens/Createpost.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import {View,StyleSheet,Text} from 'react-native';
+
+export default function Createpost(){
+    <View style={Styles.container}>
+        <Text>Createpost</Text>
+
+
+
+    </View>
+}
+
+const Styles=StyleSheet.create({
+    container:{
+        flex:1,
+    
+    }
+})
